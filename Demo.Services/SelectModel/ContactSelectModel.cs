fb07d5f9ca39981dfda19b04c2116cdf374d74cc
Xin Chen <e13333.us@gmail.com>
2021-02-07 15:11:40 -0600

    Add files via upload

---
+++ Demo.Services/SelectModel/ContactSelectModel.cs
@@ -0,0 +1,18 @@
+﻿using Demo.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Services.SelectModel
+{
+    public class ContactSelectModel : BaseSelectModel
+    {
+        public int UserID { get; set; }
+        public override string GetCacheKey()
+        {
+            //if we have redis
+            //return $"Contact_{UserID}";
+            throw new NotImplementedException();
+        }
+    }
+}
